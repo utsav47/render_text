11ce76ac04c71a116bccf23b7c505b03764a919e
Utsav Prasad <utsavprasad561@gmail.com>
2022-03-10 17:26:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+        I am learning React. My life is getting better. 
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
